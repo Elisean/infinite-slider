7f657cd59da0da56b8d62c2b6856e9a1181be3a7
Elisean <ivan.ielisieiev.97@mail.ru>
2024-06-12 11:00:21 +0300

    colors

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,15 @@
-const allImages = ['./images/image-1.png', './images/image-2.png', './images/image-3.png', './images/image-4.png'];
+const allImages = [
+     './images/image-1.png',
+     './images/image-2.png',
+     './images/image-3.png',
+     './images/image-4.png',
+     './images/image-5.png',
+     './images/image-6.jpg',
+     './images/image-7.png',
+     './images/image-8.png',
+     './images/image-9.png',
+     './images/image-10.png' 
+    ];
 
 const sliderInner = document.querySelector('.slider-inner');
 const btnNext = document.querySelector('.slider-arrow-next');
